143a36df6a63b8a77d6db7dc77ff70e058391715
rinanisssss <rina.nishizaki0124mafu@gmail.com>
2023-07-05 10:32:06 +0900

    やばいわからない

---
+++ customer.py
@@ -0,0 +1,5 @@
+ken = Customer(first_name="Ken", family_name="Tanaka")
+ken.full_name(f"first_name{ }family_name")  # "Ken Tanaka" という値を返す
+
+tom = Customer(first_name="Tom", family_name="Ford")
+tom.full_name(f"first_name{ }family_name")  # "Tom Ford" という値を返す
