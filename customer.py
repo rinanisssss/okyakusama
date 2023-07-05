01426ee78b06803696bb0ea17bb2a46282f256f5
rinanisssss <rina.nishizaki0124mafu@gmail.com>
2023-07-05 14:29:26 +0900

    c-3までできましたあああああああああああああああああああああああああああああああああ

--- customer.py
+++ customer.py
@@ -7,6 +7,22 @@ class Customer:
     def full_name(self):
         return self.first_name + " " + self.family_name  # memorizing the names
 
+    def entry_fee(self):
+        if self.age < 20:
+            return print(1000)
+        elif 20 <= self.age < 65:
+            return print(1500)
+        else:
+            return print(1200)
+
+
+ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+ken.entry_fee()
+tom = Customer(first_name="Tom", family_name="Ford", age=57)
+tom.entry_fee()
+ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+ieyasu.entry_fee()
+
 
 # # ken = Customer(first_name="Ken", family_name="Tanaka")
 # print(ken.full_name())  # "Ken Tanaka" という値を返す
@@ -14,11 +30,21 @@ class Customer:
 # print(tom.full_name())  # "Tom Ford" という値を返す
 
 
-ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
-print(ken.age)  # 15 という値を返す
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# print(ken.age)  # 15 という値を返す
 
-tom = Customer(first_name="Tom", family_name="Ford", age=57)
-print(tom.age)  # 57 という値を返す
+# tom = Customer(first_name="Tom", family_name="Ford", age=57)
+# print(tom.age)  # 57 という値を返す
 
-ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
-print(ieyasu.age)  # 73 という値を返す
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# print(ieyasu.age)  # 73 という値を返す
+
+
+# ken = Customer(first_name="Ken", family_name="Tanaka", age=15)
+# ken.entry_fee()  # 1000 という値を返す
+
+# tom = Customer(first_name="Tom", family_name="Ford", age= 57)
+# tom.entry_fee() # 1500 という値を返す
+
+# ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+# ieyasu.entry_fee() # 1200 という値を返す
